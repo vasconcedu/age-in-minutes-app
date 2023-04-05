d228855b0a446b24cb2f45e5aced323293aa19c5
Eduardo Vasconcelos <>
2023-04-05 13:39:14 -0300

    Change age and date setters to custom ones

--- app/src/main/java/com/example/ageinminutes/MainActivity.kt
+++ app/src/main/java/com/example/ageinminutes/MainActivity.kt
@@ -3,23 +3,29 @@ package com.example.ageinminutes
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import kotlin.properties.Delegates
 
 class MainActivity : AppCompatActivity() {
+
+    private var date: String = "" // TODO change to actual date type
+        set (value) {
+            field = value
+            val dateView = findViewById<TextView>(R.id.selected_date)
+            dateView.text = value
+        }
+
+    private var age: Int = 0
+        set(value) {
+            field = value
+            val ageView = findViewById<TextView>(R.id.your_age_in_minutes)
+            ageView.text = value.toString()
+        }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        setDate("TODO")
-        setAge(0)
-    }
-
-    private fun setDate(date: String) { // TODO change to actual date type
-        val dateView = findViewById<TextView>(R.id.selected_date)
-        dateView.text = date
-    }
-
-    private fun setAge(age: Int) {
-        val ageView = findViewById<TextView>(R.id.your_age_in_minutes)
-        ageView.text = age.toString()
+        date = "TODO"
+        age = 0
     }
 }

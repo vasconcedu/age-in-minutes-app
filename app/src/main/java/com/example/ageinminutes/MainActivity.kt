f4252c448502e0ebb0620a8eeb9e4a51cff579cd
Eduardo Vasconcelos <>
2023-04-05 20:43:46 -0300

    Add age in minutes feature

--- app/src/main/java/com/example/ageinminutes/MainActivity.kt
+++ app/src/main/java/com/example/ageinminutes/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import java.text.SimpleDateFormat
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
@@ -24,28 +25,49 @@ class MainActivity : AppCompatActivity() {
         }
 
     private lateinit var calendar: Calendar
+    private var yearToday: Int = 0
+    private var monthToday: Int = 0
+    private var dayToday: Int = 0
 
+    // This method formats given Calendar date as "MM/dd/yyyy"
     private fun formatDate(year: Int, month: Int, day: Int): String {
-        return "$month/$day/$year"
+        return "${
+            String.format("%02d", month + 1)
+        }/${
+            String.format("%02d", day)
+        }/${
+            String.format("%04d", year)
+        }"
     }
 
+    // This method returns the number of minutes since given date
     private fun minutesSince(year: Int, month: Int, day: Int): Long {
-        return 0L // TODO implement
+        val sdf = SimpleDateFormat("MM/dd/yyyy")
+        val selectedDateSdf = sdf.parse("$month/$day/$year")?.time
+        val todaySdf = sdf.parse("$monthToday/$dayToday/$yearToday")?.time
+        return (todaySdf!! - selectedDateSdf!!) / 60000 // Dividing by 60000 converts ms to minutes
     }
 
     private fun setupButton() {
         val button = findViewById<Button>(R.id.select_date_button)
+
+        // When the button is clicked a DatePickerDialog is shown
         button.setOnClickListener {
-            DatePickerDialog(
+            val dpd = DatePickerDialog(
                 this,
-                { _, year, month, day ->
-                    date = formatDate(year, month + 1, day)
-                    age = minutesSince(year, month, day)
+                { _, selectedYear, selectedMonth, selectedDay ->
+                    // When a date is selected:
+                    //  1. The date view is updated accordingly
+                    //  2. The age in minutes is calculated and the age view is updated accordingly
+                    date = formatDate(selectedYear, selectedMonth, selectedDay)
+                    age = minutesSince(selectedYear, selectedMonth, selectedDay)
                 },
-                calendar.get(Calendar.YEAR),
-                calendar.get(Calendar.MONTH),
-                calendar.get(Calendar.DAY_OF_MONTH)
-            ).show()
+                yearToday,
+                monthToday,
+                dayToday
+            )
+            dpd.datePicker.maxDate = System.currentTimeMillis()
+            dpd.show()
         }
     }
 
@@ -53,15 +75,14 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Need this to get current date
+        // Get current date
         calendar = Calendar.getInstance()
+        yearToday = calendar.get(Calendar.YEAR)
+        monthToday = calendar.get(Calendar.MONTH)
+        dayToday = calendar.get(Calendar.DAY_OF_MONTH)
 
         // Initial date is today
-        date = formatDate(
-            calendar.get(Calendar.YEAR),
-            calendar.get(Calendar.MONTH) + 1,
-            calendar.get(Calendar.DAY_OF_MONTH)
-        )
+        date = formatDate(yearToday, monthToday, dayToday)
 
         // Initial age is 0
         age = 0L
